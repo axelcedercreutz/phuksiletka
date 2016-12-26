87e70903c2f0881a796211c2985d32c8c3b4004a
axelcedercreutz <axel@Axel-MacBook.local>
2016-12-26 22:45:54 +0200

    phuksit ja bollinger kuvat toimii

--- src/game/View.scala
+++ src/game/View.scala
@@ -22,7 +22,6 @@ class Window extends PApplet {
   private var gameTrue = false
   private var helpTrue = false
   
-  
   override def settings() = {
     size(windowWidth * blockSize, windowHeight * blockSize)
   }
@@ -83,11 +82,15 @@ class Window extends PApplet {
     }
     for(i <- 0 until game.snakeX.size) {
       fill(0,255,0)
-      rect(game.snakeX(i) * blockSize,game.snakeY(i) * blockSize, blockSize, blockSize)
+      val phuksi = loadImage("photos/nerd.png")
+      image(phuksi, game.snakeX(i) * blockSize, game.snakeY(i) * blockSize, blockSize, blockSize)
+      //rect(game.snakeX(i) * blockSize,game.snakeY(i) * blockSize, blockSize, blockSize)
     }
     if(!game.gameOver) {
       fill(255,0,0)
-      rect(game.appleX * blockSize,game.appleY * blockSize, blockSize, blockSize)
+      val bollinger = loadImage("photos/bollinger.png")
+      image(bollinger,game.appleX * blockSize,game.appleY * blockSize, blockSize, blockSize)
+      //rect(game.appleX * blockSize,game.appleY * blockSize, blockSize, blockSize)
       if(frameCount % 2 == 0) {
         game.moveSnake()
       }
